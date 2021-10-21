e6dfaaed6d85b5dabe77958322dc33fd324146cb
Sijia Wang <sijiaoh@outlook.com>
2021-10-22 06:43:15 +0900

    Refactor index page.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,5 @@
-import {NextPage} from 'next';
 import dynamic from 'next/dynamic';
 
-const Component = dynamic(
-  async () => (await import('src/components/Home')).Home,
-  {ssr: false}
-);
-
-const Hello: NextPage = () => {
-  return <Component />;
-};
-
-export default Hello;
+export default dynamic(async () => (await import('src/components/Home')).Home, {
+  ssr: false,
+});

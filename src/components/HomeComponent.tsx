047f44f340caa2e27d2b5a28081429af86fb6eb3
Sijia Wang <sijiaoh@outlook.com>
2021-10-28 22:05:02 +0900

    Remove unnecessary codes.

--- src/components/HomeComponent.tsx
+++ src/components/HomeComponent.tsx
@@ -1,32 +1,17 @@
 import type {NextPage} from 'next';
 import Head from 'next/head';
-import {CounterComponent} from './CounterComponent';
-import {useHelloQuery} from 'src/apollo';
-import {Link} from 'src/utils/Link';
 
 export const HomeComponent: NextPage = () => {
-  const {data} = useHelloQuery();
-
   return (
     <div>
       <Head>
         <title>ohutils.com</title>
-        <meta
-          name="description"
-          content="next.js + typescript + graphql + apollo + reactive-class template"
-        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <h1>
         Welcome to <a href="https://github.com/sijiaoh/ohutils">ohutils.com!</a>
       </h1>
-
-      <Link href="#">
-        <a css={{color: 'red'}}>{data?.hello}</a>
-      </Link>
-
-      <CounterComponent css={{marginTop: '1em'}} />
     </div>
   );
 };
--- src/graphql/resolvers/index.ts
+++ src/graphql/resolvers/index.ts
@@ -1,2 +1,1 @@
-export * from './HelloResolver';
 export * from './UserResolver';

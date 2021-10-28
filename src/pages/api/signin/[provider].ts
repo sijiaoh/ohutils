ca0f1d6b3a2fabf7d266ff432ffb0982d04f77b0
Sijia Wang <sijiaoh@outlook.com>
2021-10-28 14:19:22 +0900

    Use utils/handler in signin apis.

--- src/pages/api/signin/[provider].ts
+++ src/pages/api/signin/[provider].ts
@@ -1,10 +1,7 @@
-import nextConnect from 'next-connect';
 import passport from 'passport';
 import {signInMiddleware} from 'src/auth/signInMiddleware';
-import {databaseMiddleware} from 'src/database/databaseMiddleware';
+import {handler} from 'src/utils/handler';
 
-const handler = nextConnect();
-handler.use(databaseMiddleware);
 handler.use(passport.initialize());
 handler.get(signInMiddleware);
 export default handler;

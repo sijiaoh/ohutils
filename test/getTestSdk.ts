5cc042ec63219d596408fa72972445ae0e46336f
Sijia Wang <sijiaoh@outlook.com>
2021-10-29 22:02:50 +0900

    Add getAgent.

--- test/getTestSdk.ts
+++ test/getTestSdk.ts
@@ -1,19 +1,13 @@
-import express from 'express';
 import {print} from 'graphql';
-import request, {SuperAgentTest} from 'supertest';
+import {SuperAgentTest} from 'supertest';
 import {getSdk} from './generated/generic-sdk';
-import {getGraphqlHandler} from 'src/graphql/getGraphqlHandler';
+import {getAgent} from './getAgent';
 
 export const getTestSdk = (agent?: SuperAgentTest) => {
-  const app = express();
-  app.post('/api/graphql', (req, res, next) => {
-    void getGraphqlHandler()(req, res).then(next).catch(next);
-  });
-
-  const ag = agent || request.agent(app);
+  agent = agent || getAgent();
 
   return getSdk(async (doc, vars) => {
-    const res = await ag
+    const res = await agent!
       .post('/api/graphql')
       .send({query: print(doc), variables: vars});
     if (res.error) throw res.error;

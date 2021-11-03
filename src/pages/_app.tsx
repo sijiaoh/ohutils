0370beba13add439b6741ee0365b996de4aad243
Sijia Wang <sijiaoh@outlook.com>
2021-11-04 01:58:15 +0900

    Add sanitize.css.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,7 @@
+import 'sanitize.css';
+import 'sanitize.css/forms.css';
+import 'sanitize.css/typography.css';
+
 import {ApolloProvider} from '@apollo/client';
 import type {AppProps} from 'next/app';
 import {apolloClient} from 'src/apollo';

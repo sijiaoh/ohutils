030ed4710ddad2ea4d98e39f6a24c8c2a1b0dc0c
Sijia Wang <sijiaoh@outlook.com>
2021-11-03 13:05:33 +0900

    Add analytics.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,11 @@
 import {ApolloProvider} from '@apollo/client';
 import type {AppProps} from 'next/app';
 import {apolloClient} from 'src/apollo';
+import {useAnalytics} from 'src/utils/analytics';
 
 const MyApp = ({Component, pageProps}: AppProps) => {
+  useAnalytics();
+
   return (
     <ApolloProvider client={apolloClient}>
       <Component {...pageProps} />

5479e6d511d395a180f954cb331431e1b7363a61
Sijia Wang <sijiaoh@outlook.com>
2021-11-04 00:04:00 +0900

    Cosme container.

--- src/components/ContainerComponent.tsx
+++ src/components/ContainerComponent.tsx
@@ -3,7 +3,16 @@ import {PropsWithChildren} from 'react';
 export const ContainerComponent = ({children}: PropsWithChildren<{}>) => {
   return (
     <div css={{display: 'flex', justifyContent: 'center'}}>
-      <div css={{width: '720px'}}>{children}</div>
+      <div
+        css={{
+          '@media (min-width: 720px)': {
+            minWidth: '720px',
+          },
+          minWidth: '90vw',
+        }}
+      >
+        {children}
+      </div>
     </div>
   );
 };

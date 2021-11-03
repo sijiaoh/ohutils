a98a6c941191160963c3fbd4f0a2444fd2509b20
Sijia Wang <sijiaoh@outlook.com>
2021-11-03 23:25:36 +0900

    Add global style.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,19 @@
 import {ApolloProvider} from '@apollo/client';
 import type {AppProps} from 'next/app';
 import {apolloClient} from 'src/apollo';
+import {GlobalStyleComponent} from 'src/components/GlobalStyleComponent';
 import {useAnalytics} from 'src/utils/analytics';
 
 const MyApp = ({Component, pageProps}: AppProps) => {
   useAnalytics();
 
   return (
-    <ApolloProvider client={apolloClient}>
-      <Component {...pageProps} />
-    </ApolloProvider>
+    <>
+      <GlobalStyleComponent />
+      <ApolloProvider client={apolloClient}>
+        <Component {...pageProps} />
+      </ApolloProvider>
+    </>
   );
 };
 

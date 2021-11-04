9e395f0871f1978aeeed4750b42eb711fdd6b326
Sijia Wang <sijiaoh@outlook.com>
2021-11-05 02:33:33 +0900

    Add react-bootstrap.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,8 @@ import 'sanitize.css';
 import 'sanitize.css/forms.css';
 import 'sanitize.css/typography.css';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import {ApolloProvider} from '@apollo/client';
 import type {AppProps} from 'next/app';
 import {apolloClient} from 'src/apollo';

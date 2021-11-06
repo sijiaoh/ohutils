7427de5fa6b53ac4d1488cef33b7f2253d6b3291
Sijia Wang <sijiaoh@outlook.com>
2021-11-06 11:59:54 +0900

    Add watchGenerate.

--- src/graphql/resolvers/HelloResolver.ts
+++ src/graphql/resolvers/HelloResolver.ts
@@ -5,7 +5,7 @@ import {Context} from 'src/utils/Context';
 
 @Resolver()
 export class HelloResolver {
-  @Query(() => String)
+  @Query(() => String, {nullable: true})
   hello(@Ctx() {req, res}: Context) {
     const helloCount = parseInt(req.cookies.helloCount || '0') + 1;
     res.cookie('helloCount', helloCount.toString());

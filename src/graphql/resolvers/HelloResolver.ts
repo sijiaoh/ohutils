5fc02abf198d255b1437551d8df4235181034cc7
Sijia Wang <sijiaoh@outlook.com>
2021-11-06 12:06:22 +0900

    Fix hello query response type.

--- src/graphql/resolvers/HelloResolver.ts
+++ src/graphql/resolvers/HelloResolver.ts
@@ -5,7 +5,7 @@ import {Context} from 'src/utils/Context';
 
 @Resolver()
 export class HelloResolver {
-  @Query(() => String, {nullable: true})
+  @Query(() => String)
   hello(@Ctx() {req, res}: Context) {
     const helloCount = parseInt(req.cookies.helloCount || '0') + 1;
     res.cookie('helloCount', helloCount.toString());

53ed12496d6002495eab0e5c82d0bc97dc5bc29e
Sijia Wang <sijiaoh@outlook.com>
2021-11-09 04:22:48 +0900

    Fix pages export.

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -1,6 +1,4 @@
 import {Breadcrumb} from 'src/components/BreadcrumbListComponent';
-import {SignInComponent} from 'src/components/SignInComponent';
-import {withNotAuth} from 'src/hocs/withNotAuth';
 import {toCsrPage} from 'src/utils/toCsrPage';
 
 export const signInTitle = 'ログイン';
@@ -10,4 +8,6 @@ export const signInBreadcrumb: Breadcrumb = {
   path: signInPath,
 };
 
-export default toCsrPage(withNotAuth(SignInComponent));
+export default toCsrPage(import('src/components/SignInComponent'), {
+  requireAuth: false,
+});

1ac9520fe65b60b25a4bd4624ecab85a81907992
Sijia Wang <sijiaoh@outlook.com>
2021-11-09 04:47:01 +0900

    Remove global style.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,19 +5,15 @@ import 'sanitize.css/typography.css';
 import {ApolloProvider} from '@apollo/client';
 import type {AppProps} from 'next/app';
 import {apolloClient} from 'src/apollo';
-import {GlobalStyleComponent} from 'src/components/GlobalStyleComponent';
 import {useAnalytics} from 'src/utils/analytics';
 
 const MyApp = ({Component, pageProps}: AppProps) => {
   useAnalytics();
 
   return (
-    <>
-      <GlobalStyleComponent />
-      <ApolloProvider client={apolloClient}>
-        <Component {...pageProps} />
-      </ApolloProvider>
-    </>
+    <ApolloProvider client={apolloClient}>
+      <Component {...pageProps} />{' '}
+    </ApolloProvider>
   );
 };
 

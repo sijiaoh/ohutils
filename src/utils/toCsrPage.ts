21c13def096bfdb002e0ea46996bef8501471601
Sijia Wang <sijiaoh@outlook.com>
2021-11-09 04:21:12 +0900

    Add auth option to toCsrPage.

--- src/utils/toCsrPage.ts
+++ src/utils/toCsrPage.ts
@@ -1,6 +1,23 @@
 import {NextPage} from 'next';
 import dynamic from 'next/dynamic';
+import {withAuth} from 'src/hocs/withAuth';
+import {withNotAuth} from 'src/hocs/withNotAuth';
 
-export const toCsrPage = (module: Promise<{[key: string]: NextPage}>) => {
-  return dynamic(async () => Object.values(await module)[0]!, {ssr: false});
+export const toCsrPage = (
+  module: Promise<{[key: string]: NextPage}>,
+  {requireAuth}: {requireAuth?: boolean} = {}
+) => {
+  return dynamic(
+    async () => {
+      const Page = Object.values(await module)[0]!;
+      if (requireAuth === undefined) {
+        return Page;
+      } else if (requireAuth) {
+        return withAuth(Page);
+      } else {
+        return withNotAuth(Page);
+      }
+    },
+    {ssr: false}
+  );
 };

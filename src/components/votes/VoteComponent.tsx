621c930ab04e294aea70a520b075da8334b0aa97
Sijia Wang <sijiaoh@outlook.com>
2021-11-08 10:39:27 +0900

    Add graph bar to vote page.

--- src/components/votes/VoteComponent.tsx
+++ src/components/votes/VoteComponent.tsx
@@ -1,6 +1,6 @@
 import {useListen} from '@reactive-class/react';
 import {useRouter} from 'next/dist/client/router';
-import {useEffect, useRef} from 'react';
+import {useEffect, useRef, useState} from 'react';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import {BreadcrumbListComponent} from '../BreadcrumbListComponent';
@@ -16,6 +16,14 @@ export const VoteComponent = () => {
   const {id} = router.query;
   const vote = useRef(new Vote()).current;
   const voteData = useListen(vote, ({data}) => data);
+  const totalNumberOfVotes = useListen(vote, ({data}) => {
+    return (
+      data?.voteOptions.reduce(
+        (sum, voteOption) => sum + voteOption.numberOfVotes,
+        0
+      ) || 0
+    );
+  });
 
   useEffect(() => {
     if (typeof id !== 'string') return;
@@ -41,8 +49,25 @@ export const VoteComponent = () => {
       <ul>
         {voteData.voteOptions.map(voteOption => (
           <li key={voteOption.id} css={{display: 'flex', alignItems: 'center'}}>
-            <div>{voteOption.name}</div>
-            <div>{voteOption.numberOfVotes}</div>
+            <div css={{width: '15%', padding: '0 0.2em'}}>
+              {voteOption.name}
+            </div>
+            <div css={{width: '5%', padding: '0 0.2em'}}>
+              {voteOption.numberOfVotes}
+            </div>
+            <div css={{flex: 1, padding: '0 1em'}}>
+              {totalNumberOfVotes > 0 && (
+                <div
+                  css={{
+                    backgroundColor: 'blue',
+                    width: `${
+                      (voteOption.numberOfVotes / totalNumberOfVotes) * 100
+                    }%`,
+                    height: '1em',
+                  }}
+                />
+              )}
+            </div>
             <Button
               onClick={async () => {
                 await vote.voteTo(voteOption.id);

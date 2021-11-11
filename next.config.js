d711924329caba5e48dff4dd3c1a09b704e9dd39
Sijia Wang <sijiaoh@outlook.com>
2021-11-11 20:27:30 +0900

    Add deploy command.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
-/** @type {import('next').NextConfig} */
+let env = undefined;
 
+if (process.env.DEPLOY === 'true') {
+  const fs = require('fs');
+  const envJson = fs.readFileSync('.env/production.json');
+  env = JSON.parse(envJson);
+}
+
+/** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  env,
   webpack: config => {
     // eslint-disable-next-line node/no-unpublished-require
     const CircularDependencyPlugin = require('circular-dependency-plugin');

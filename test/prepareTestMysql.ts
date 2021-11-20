0a71d8d40b7c8df88d5b076f7376f9bf3bad38ba
Sijia Wang <sijiaoh@outlook.com>
2021-11-20 17:17:07 +0900

    Refactor prepareTestMysql.

--- test/prepareTestMysql.ts
+++ test/prepareTestMysql.ts
@@ -4,58 +4,30 @@ import {PrismaClient} from '.prisma/client';
 import {getDatabaseName} from 'src/database/getDatabaseName';
 import {getDatabaseUrl} from 'src/database/getDatabaseUrl';
 import {getPrisma, setPrisma} from 'src/database/prisma';
-import {serverEnv} from 'src/generated/serverEnv';
+
+// From: https://github.com/sijiaoh/docker-mysql/blob/main/src/index.ts#L8
+const mysqlRootPassword = 'docker-mysql-root-password';
 
 export const prepareTestMysql = () => {
   const databaseName = `${getDatabaseName()}-${v4()}`.replace(/-/g, '_');
+  const databaseUrl = getDatabaseUrl({
+    user: 'root',
+    pass: mysqlRootPassword,
+    name: databaseName,
+  });
 
-  beforeAll(async () => {
-    await execa(
-      'yarn',
-      [
-        'docker-mysql',
-        'prepare',
-        serverEnv.DB_VERSION,
-        databaseName,
-        '--userName',
-        serverEnv.DB_USER,
-        '--password',
-        serverEnv.DB_PASS,
-      ],
-      {env: process.env, stdio: 'inherit'}
-    );
-
-    await execa('yarn', ['prisma', 'migrate'], {
-      env: {...process.env, DB_URL: getDatabaseUrl(databaseName)},
-    });
-
-    setPrisma(
-      new PrismaClient({datasources: {db: {url: getDatabaseUrl(databaseName)}}})
-    );
+  beforeAll(() => {
+    setPrisma(new PrismaClient({datasources: {db: {url: databaseUrl}}}));
   });
 
   afterAll(async () => {
+    await getPrisma().$executeRawUnsafe(`drop database ${databaseName};`);
     await getPrisma().$disconnect();
-
-    await execa(
-      'yarn',
-      [
-        'docker-mysql',
-        'rm',
-        serverEnv.DB_VERSION,
-        databaseName,
-        '--userName',
-        serverEnv.DB_USER,
-        '--password',
-        serverEnv.DB_PASS,
-      ],
-      {env: process.env, stdio: 'inherit'}
-    );
   });
 
   beforeEach(async () => {
     await execa('yarn', ['prisma', 'migrate', 'reset', '--force'], {
-      env: {...process.env, DB_URL: getDatabaseUrl(databaseName)},
+      env: {...process.env, DB_URL: databaseUrl},
     });
   });
 };

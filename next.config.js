0a50c4c63431dca2c99201cff8644c4e826d87f1
Sijia Wang <sijiaoh@outlook.com>
2021-11-22 03:49:10 +0900

    Fix prisma deploy error.

--- next.config.js
+++ next.config.js
@@ -8,7 +8,8 @@ if (process.env.ANALYZE === 'true') {
 /** @type {import('next').NextConfig} */
 module.exports = wrapper({
   reactStrictMode: true,
-  webpack: config => {
+
+  webpack: (config, {dev, isServer}) => {
     // eslint-disable-next-line node/no-unpublished-require
     const CircularDependencyPlugin = require('circular-dependency-plugin');
     config.plugins = [
@@ -19,6 +20,12 @@ module.exports = wrapper({
         cwd: process.cwd(),
       }),
     ];
+
+    // TODO: 下記のIssueの動向次第で削除する。
+    // https://github.com/serverless-nextjs/serverless-next.js/issues/843#issuecomment-837291066
+    // From: https://github.com/millsp/prisma-serverless-nextjs/blob/main/next.config.js#L38
+    if (!dev && isServer) config.externals.push('_http_common');
+
     return config;
   },
 });
--- src/database/prisma.ts
+++ src/database/prisma.ts
@@ -1,3 +1,6 @@
 import {PrismaClient} from '@prisma/client';
+import {serverEnv} from 'src/generated/serverEnv';
 
+// For lambda deploy.
+process.env.DB_URL = serverEnv.DB_URL;
 export const prisma = new PrismaClient();

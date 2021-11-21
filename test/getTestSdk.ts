9c2af3ca36b251557c9aa02af29a65ead57bec6f
Sijia Wang <sijiaoh@outlook.com>
2021-11-21 14:36:51 +0900

    Remove test/generated/generic-sdk.

--- test/getTestSdk.ts
+++ test/getTestSdk.ts
@@ -1,19 +1,32 @@
+import {ApolloClient, NetworkStatus} from '@apollo/client';
 import {print} from 'graphql';
 import type {SuperAgentTest} from 'supertest';
-import {getSdk} from './generated/generic-sdk';
 import {getAgent} from './getAgent';
+import {getSdk} from 'src/generated/graphql-apollo';
 
 export const getTestSdk = (agent?: SuperAgentTest) => {
   agent = agent || getAgent();
 
-  return getSdk(async (doc, vars) => {
+  const query: ApolloClient<unknown>['query'] = async ({query, variables}) => {
     const res = await agent!
       .post('/api/graphql')
-      .send({query: print(doc), variables: vars});
+      .send({query: print(query), variables});
 
-    if (res.body.errors) throw res.body.errors;
-    if (res.error) throw res.error;
+    return {
+      data: res.body.data,
+      errors: res.body.errors,
+      error: res.body.error,
+      loading: false,
+      networkStatus: NetworkStatus.ready,
+    };
+  };
 
-    return res.body.data;
-  });
+  return getSdk({
+    query: async props => {
+      return query(props);
+    },
+    mutate: async props => {
+      return query({...props, query: props.mutation});
+    },
+  } as Pick<ApolloClient<unknown>, 'query' | 'mutate'> as ApolloClient<unknown>);
 };
